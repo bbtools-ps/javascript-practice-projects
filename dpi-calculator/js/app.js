a33c8dfa1c62cc2dcee9c6cde3cc8c6a3e8be810
bogdanovicb <b.bogdanovic@case-3d.com>
2021-11-17 18:45:11 +0100

    bugfix: calculations on input.

--- dpi-calculator/js/app.js
+++ dpi-calculator/js/app.js
@@ -1,40 +1,79 @@
 (function () {
-    // get values
+    // set variables
     const viewingDistance = document.querySelector("#viewing-distance");
     const dpi = document.querySelector("#dpi");
-    const units = [
-        "pixels",
-        "inches",
-        "centimeters",
-        "millimeters",
-        "points",
-        "picas",
-    ];
+    const units = ["meters", "centimeters", "millimeters", "feet", "inches"];
     const unitsSelect = document.querySelector("#units");
     let unitsContent = "";
+    const unitValue = document.querySelector("#unit");
 
-    // calculate dpi and viewingDistance based on inputs
-    function calc() {
-        dpi.value = Math.floor((180 / viewingDistance.value) * 100);
-        viewingDistance.value = Math.floor((180 / dpi.value) * 100);
+    // units abbreviation
+    function unitsAbbreviation(unit) {
+        switch (unit) {
+            case "meters":
+                return "m";
+            case "centimeters":
+                return "cm";
+            case "millimeters":
+                return "mm";
+            case "feet":
+                return "ft";
+            case "inches":
+                return "in";
+            default:
+                return "";
+        }
     }
 
-    // populate units content
+    function unitsConverter(unit, value) {
+        switch (unit) {
+            case "m":
+                return parseFloat(value) * 100;
+            case "cm":
+                return parseFloat(value);
+            case "mm":
+                return parseFloat(value) / 10;
+            case "ft":
+                return parseFloat(value) / 0.03280839895;
+            case "in":
+                return parseFloat(value) / 0.3937007874;
+        }
+    }
+
+    // calculate dpi based on viewing distance
+    function calcDpi(num) {
+        // calculation is done in cm so it has to converted to a proper value
+        let viewingDistanceValue = unitsConverter(unitValue.innerHTML, num);
+        return Math.floor((180 / viewingDistanceValue) * 100);
+    }
+
+    // calculate viewing distance based on dpi
+    function calcViewingDistance(num) {
+        // calculation is done in cm so it has to converted to a proper value
+        let dpiValue = unitsConverter(unitValue.innerHTML, num);
+        return Math.floor((180 / dpiValue) * 100);
+    }
+
+    // set event listeners
+    viewingDistance.addEventListener("input", function () {
+        dpi.value = calcDpi(this.value);
+    });
+
+    dpi.addEventListener("input", function () {
+        viewingDistance.value = calcViewingDistance(this.value);
+    });
+
+    unitsSelect.addEventListener("change", function () {
+        unitValue.innerHTML = unitsAbbreviation(this.value);
+        dpi.value = calcDpi(viewingDistance.value);
+    });
+
+    // UI
     units.forEach(function (item) {
         unitsContent += `<option value="${item}">${item}</option>`;
     });
     unitsSelect.innerHTML = unitsContent;
-
-    // set event listeners
-    viewingDistance.addEventListener("input", calc);
-    dpi.addEventListener("input", calc);
-    unitsSelect.addEventListener("change", function () {
-        const unitValue = document.querySelector("#unit");
-        unitValue.innerHTML = this.value;
-    });
-
-    // default values
-    viewingDistance.value = 200;
-    calc();
     unitsSelect.value = units[2];
+    viewingDistance.value = 200;
+    dpi.value = calcDpi(viewingDistance.value);
 })();
